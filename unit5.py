57d5b443bf3d6b880a5f4c526e070cbfb533cf2c
guillermo <porregu@ssfs.org>
2019-10-21 14:14:49 -0400

    make unit 5 project

--- unit5.py
+++ unit5.py
@@ -1,14 +1,22 @@
 import random
-user_games = input(int(" how many games you wanna play "))
+user_games = int(input(" how many games you wanna play "))
 dices1= random.randint(1,6)
 dices2= random.randint(1,6)
 total_dices=dices1+dices2
+print(total_dices)
 if total_dices==7 or total_dices== 11:
-    print("you win")
+    print("user win")
 elif total_dices==2 or total_dices== 3 or total_dices== 12:
-    print("you lose ")
+    print("user lose ")
 else:
-    dices1 = random.randint(1, 6)
-    dices2 = random.randint(1, 6)
-    total2_dices= dices2+dices1
-    if total_dices==total2_dices
+    while True:
+        dices1 = random.randint(1, 6)
+        dices2 = random.randint(1, 6)
+        total2_dices= dices2+dices1
+        print(total2_dices)
+        if total_dices==total2_dices:
+            print("user won")
+            break
+        if total2_dices==7:
+            print("user lose")
+            break
