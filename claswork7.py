4d96e052c1b1916e9d4257a818edbb343c47f7f9
guillermo <porregu@ssfs.org>
2019-10-22 15:05:30 -0400

    finish unit 5 project and start claswork 7

---
+++ claswork7.py
@@ -0,0 +1,5 @@
+import random
+user_number=int(input("what integir you want ?"))
+for x in range(user_number):
+    letter=input("how many times")
+    if letter=="q":
--- unit5.py
+++ unit5.py
@@ -1,22 +1,38 @@
 import random
-user_games = int(input(" how many games you wanna play "))
-dices1= random.randint(1,6)
-dices2= random.randint(1,6)
-total_dices=dices1+dices2
-print(total_dices)
-if total_dices==7 or total_dices== 11:
-    print("user win")
-elif total_dices==2 or total_dices== 3 or total_dices== 12:
-    print("user lose ")
-else:
-    while True:
-        dices1 = random.randint(1, 6)
-        dices2 = random.randint(1, 6)
-        total2_dices= dices2+dices1
-        print(total2_dices)
-        if total_dices==total2_dices:
-            print("user won")
-            break
-        if total2_dices==7:
-            print("user lose")
-            break
+def role_dices():
+    """
+    this function makes the role of the dices reapet and make less codes
+    :return:
+    """
+    dices1 = random.randint(1, 6)#role dice and pick a random number
+    dices2 = random.randint(1, 6)#role dice and pick a random number
+    total_dices = dices1 + dices2# make the total of the two dices
+    return total_dices
+user_games = int(input("how many games you want play today? "))
+total_wins=0#for later calculate the number of wins
+total_lose=0#for later calculate the number of loses
+for x in range (user_games):
+
+    total_dices=role_dices()
+    #print("this is your totlat dices",total_dices)
+    if total_dices==7 or total_dices== 11:
+        total_wins=total_wins+1
+        #print("user win")
+    elif total_dices==2 or total_dices== 3 or total_dices== 12:
+        total_lose=total_lose+1
+        #print("user lose ")
+    else:
+        while True:
+            total2_dices= role_dices()
+            #print("this is your total for your second try",total2_dices)
+            if total_dices==total2_dices:
+                total_wins = total_wins + 1
+                #print("user won")
+                break
+            if total2_dices==7:
+                total_lose = total_lose + 1
+                #print("user lose")
+                break
+print(" the user win",total_wins,"times")
+print(total_wins/user_games*100,"%","of wins")
+print(total_lose/user_games*100,"%","of loses")
